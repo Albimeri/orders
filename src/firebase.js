34bd99ae8403f56edf4746eb6e4904c43207d49c
Alb Imeri <alb.imeri@solaborate.com>
2021-03-29 00:13:25 +0200

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,13 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-const app = firebase.initializeApp({
-  //   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  //   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  //   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  //   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  //   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  //   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  apiKey: "AIzaSyAtGbqupi0UOiljI6KdWb2NEZAdzd0UKcQ",
-  authDomain: "auth-development-env.firebaseapp.com",
-  projectId: "auth-development-env",
-  storageBucket: "auth-development-env.appspot.com",
-  messagingSenderId: "254554328802",
-  appId: "1:254554328802:web:611056b3632eb37a1098bb",
+const app = firebase.initializeApp({ 
+  apiKey: "AIzaSyA8N2XJRf6iQ2tUJT3LtzUCcVzFbtJgl2w",
+  authDomain: "orderinteams.firebaseapp.com",
+  projectId: "orderinteams",
+  storageBucket: "orderinteams.appspot.com",
+  messagingSenderId: "124756348342",
+  appId: "1:124756348342:web:3d2bc3bd1cff288cccc517",
 });
 
 export const auth = app.auth();
